ed31b4bf03d0e171a6eb52a9d42daaf512adb643
Sydney <sydney3@stanford.edu>
2025-12-02 18:01:41 -0800

    more initial efforts for image generation

--- slsim/TrainingSets/Configs/example_config.py
+++ slsim/TrainingSets/Configs/example_config.py
@@ -1,5 +1,23 @@
-from scipy.stats import lognorm, multivariate_normal
+from scipy.stats import truncnorm, lognorm, multivariate_normal
 import numpy as np
+
+def RedshiftSampler(z_lens_mean,z_lens_std,z_src_mean,z_src_std):
+    """
+    Docstring for RedShiftSampler
+    
+    :param z_lens_mean: Gaussian mean for lens redshift distribution
+    :param z_lens_std: Gaussian std dev. for lens redshift distribution
+    :param z_src_mean: Gaussian mean for src redshift distribution
+    :param z_src_std: Gaussian std. dev. for src redshift distribution
+
+    Returns: (z_lens,z_src)
+    """
+
+    z_lens = truncnorm.rvs(-z_lens_mean/z_lens_std,np.inf,loc=z_lens_mean,scale=z_lens_std)
+    z_src = truncnorm.rvs((z_lens-z_src_mean)/z_src_std,np.inf,loc=z_src_mean,scale=z_src_std)
+
+    return z_lens,z_src
+
 
 sampling_blocks = [
 
@@ -15,12 +33,36 @@ sampling_blocks = [
 
     # Other deflector params from multivariate gaussian
     { 'param_keys':['deflector_gamma','deflector_e1_mass','deflector_e2_mass',
-        'deflector_e1_light','deflector_e2_light',
-        'deflector_angular_size','deflector_n_sersic'],
+        'deflector_e1_light','deflector_e2_light','deflector_mag_i',
+        'deflector_angular_size','deflector_n_sersic',
+        'los_gamma1','los_gamma2'],
     'sampler_method':multivariate_normal.rvs,
     'sampler_args':{
-        'mean':[2.,0.,0.,0.,0.,],
-        'cov':np.diag(np.asarray([])**2),
+        'mean':[2.,0.,0.,0.,0.,20.,1.,4.,0.,0.], # TODO: these are just placeholders for now...
+        'cov':np.diag(np.asarray([0.1,0.1,0.1,0.1,0.1,0.4,0.2,0.2,0.1,0.1])**2),
+        }
+   },
+
+   # Redshifts
+   {
+       'param_keys':['deflector_z','source_z'],
+       'sampler_method': RedshiftSampler,
+       'sampler_args':{
+           'z_lens_mean':0.5,
+           'z_lens_std':0.3,
+           'z_src_mean':1.5,
+           'z_src_std':0.7,
+       }
+   },
+
+   # Source params
+   { 'param_keys': ['source_ps_mag_i','source_angular_size','source_n_sersic',
+        'source_e1','source_e2','source_center_x','source_center_y',
+        'source_mag_i'],
+    'sampler_method':multivariate_normal.rvs,
+    'sampler_args':{
+        'mean':[21.,1.,2.,0.,0.,0.,0.,22.5], # TODO: these are just placeholders for now...
+        'cov':np.diag(np.asarray([1.,0.2,0.2,0.1,0.1,0.05,0.05,1.])**2),
         }
    },
 ]
--- slsim/TrainingSets/generate_images.py
+++ slsim/TrainingSets/generate_images.py
@@ -1,20 +1,34 @@
 from slsim.Deflectors.DeflectorTypes.epl_sersic import EPLSersic
 from slsim.LOS.los_individual import LOSIndividual
+from slsim.Sources.source import Source
 from slsim.Sources.SourceTypes.point_plus_extended_source import PointPlusExtendedSource
 from slsim.Lenses.lens import Lens
-
+from slsim.LsstSciencePipeline.lsst_science_pipeline import lens_inejection_fast
+from astropy.cosmology import FlatLambdaCDM
+import numpy as np
 
 # import configuration file stuff (TODO fix this...)
 from Configs.example_config import sampling_blocks as config_sampling_blocks
 
+# TODO: choose a cosmology (how to make this clearly defined by the user)
+groundtruth_cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
 # sample given setup in configuration file
 sampled_params_dict = {}
+# sample one block at a time
 for sampling_blocks in config_sampling_blocks:
+    # take a sample
     params_sample = sampling_blocks['sampler_method'](**sampling_blocks['sampler_args'])
-    for i in range (0,len(sampling_blocks['param_keys'])):
-        sampled_params_dict[sampling_blocks['param_keys'][i]] = params_sample[i]
+    # edge case for 1 param in sampling block
+    if np.isscalar(params_sample):
+        sampled_params_dict[sampling_blocks['param_keys'][0]] = params_sample
+    else: 
+        for i in range (0,len(sampling_blocks['param_keys'])):
+            sampled_params_dict[sampling_blocks['param_keys'][i]] = params_sample[i]
 
-# make a deflector
+print(sampled_params_dict)
+
+# make a deflector (captures mass & light of the object...)
 epl_sersic_required_params = ['theta_E','gamma',
     'e1_mass','e2_mass','e1_light','e2_light','mag_i','angular_size']
 
@@ -24,22 +38,37 @@ for key in epl_sersic_required_params:
         raise ValueError('Missing param: deflector_'+key)
     deflector_dict[key] = sampled_params_dict['deflector_'+key]
 
-deflector_obj = EPLSersic(deflector_dict)
+deflector_obj = EPLSersic(deflector_dict=deflector_dict,z=sampled_params_dict['deflector_z'])
 
 # make a LOS object for ext. shear
 los_required_params = ['gamma1','gamma2']
-los_dict = {}
 for key in los_required_params:
     if ('los_'+key) not in sampled_params_dict.keys():
         raise ValueError('Missing param: los_'+key)
-    los_dict[key] = sampled_params_dict['los_'+key]
-los_obj = LOSIndividual()
+gamma = [sampled_params_dict['los_gamma1'],sampled_params_dict['los_gamma2']]
+los_obj = LOSIndividual(gamma=gamma)
 
-# make a source
-source_obj = PointPlusExtendedSource()
+# make a source (captures extended & point source)
+source_required_params = ['ps_mag_i','e1','e2',
+    'center_x','center_y','mag_i','angular_size','n_sersic']
+source_dict = {}
+for key in source_required_params:
+    if ('source_'+key) not in sampled_params_dict.keys():
+        raise ValueError('Missing param: source_'+key)
+    source_dict[key] = sampled_params_dict['source_'+key]
+
+# TODO: why are angular_size and n_sersic treated differently than the other params here? 
+source_obj = Source(extended_source_type='single_sersic',
+    point_source_type='quasar',source_dict=source_dict,
+    z=sampled_params_dict['source_z'],
+    angular_size=sampled_params_dict['source_angular_size'],
+    n_sersic=sampled_params_dict['source_n_sersic'])
 
 # combine into a lens object
-lens_obj = Lens()
+lens_obj = Lens(source_class=source_obj,deflector_class=deflector_obj,
+    los_class=los_obj,cosmo=groundtruth_cosmo)
 
-# 
+# TODO: inject into DP0 co-adds using LsstSciencePipeline code...
+lens_inejection_fast(lens_pop=[lens_obj])
 
+# TODO: save in MMU .h5 / HuggingFace format 
